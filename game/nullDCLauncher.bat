9ee04a50c19ed686134d9b1162f1733f5415d6a9
TwitchPlaysDeveloper <kipor7aug@yahoo.co.uk>
2018-01-19 13:43:44 +0000

    wip nullDC support

--- game/nullDCLauncher.bat
+++ game/nullDCLauncher.bat
@@ -4,7 +4,7 @@
 set nullDCPath="C:\Users\jamie\Desktop\nullDC 1.04 r150 SM Fixes by masterchan777\nullDC.exe"
 set gamePath=""
 
-start "" %nullDCPath% %gamePath%
+start "" %nullDCPath%
 ::--fullboot
 
 exit 0
